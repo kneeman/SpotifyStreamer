d276b4fb8c84478d94f7c7ef85d272f541624bc0
Craig Knee <c_cknee@C-CKNEE.na.qualcomm.com>
2015-08-10 15:02:54 -0700

    Prior to changing from parceable to gson

--- app/src/main/java/com/knee/spotifystreamer/PlayerDialogFragment.java
+++ app/src/main/java/com/knee/spotifystreamer/PlayerDialogFragment.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by c_cknee on 8/5/2015.
@@ -16,23 +20,25 @@ public class PlayerDialogFragment extends DialogFragment{
     private boolean mIsLargeLayout;
     private String songUrl;
     private static final String KEY_LARGE_LAYOUT = "keyLargeLayout", KEY_TRACK_URL = "keyTrackUrl";
+    private TextView mArtistName, mAlbumName, mTrackName, mLeftTrackTime, mRightTrackTime;
+    private ImageView mAlbumImage;
+    private ImageButton mPreviousTrackButton, mNextTrackButton, mPlayPauseButton;
+    private SeekBar mSeekBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view  = inflater.inflate(R.layout.fragment_track_player, container, false);
+        bindControls(view);
         Bundle arguments = getArguments();
         mIsLargeLayout = arguments.getBoolean(KEY_LARGE_LAYOUT, false);
         songUrl = arguments.getString(KEY_TRACK_URL, "");
-        return inflater.inflate(R.layout.fragment_track_player, container, false);
+        return view;
     }
 
-    /** The system calls this only when creating the layout in a dialog. */
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // The only reason you might override this method when using onCreateView() is
-        // to modify any dialog characteristics. For example, the dialog includes a
-        // title by default, but your custom layout might not need it. So here you can
-        // remove the dialog title, but you must call the superclass to get the Dialog.
         Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         return dialog;
@@ -45,5 +51,18 @@ public class PlayerDialogFragment extends DialogFragment{
         args.putString(KEY_TRACK_URL, pSongUrl);
         frag.setArguments(args);
         return frag;
+    }
+
+    private void bindControls(View view) {
+        mArtistName = (TextView) view.findViewById(R.id.track_group_name);
+        mAlbumName =  (TextView) view.findViewById(R.id.track_album_name);
+        mTrackName = (TextView) view.findViewById(R.id.track_track_name);
+        mLeftTrackTime = (TextView) view.findViewById(R.id.track_left_track_time);
+        mRightTrackTime = (TextView) view.findViewById(R.id.track_right_track_time);
+        mAlbumImage = (ImageView) view.findViewById(R.id.track_album_image);
+        mPreviousTrackButton = (ImageButton) view.findViewById(R.id.track_previous_button);
+        mNextTrackButton = (ImageButton) view.findViewById(R.id.track_next_button);
+        mPlayPauseButton = (ImageButton) view.findViewById(R.id.track_play_pause_button);
+        mSeekBar = (SeekBar) view.findViewById(R.id.track_seek_bar);
     }
 }
